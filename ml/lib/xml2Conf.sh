522a002cfc38f9e80f2b56445402e4f8c189928d
Li Xin <xgli@hp8x-48.cs.helsinki.fi>
2018-01-31 12:12:48 +0200

    environment ds and ml

---
+++ ml/lib/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/home/xgli/anaconda3/envs/ml/lib"
+XML2_LIBS="-lxml2 -L/home/xgli/anaconda3/envs/ml/lib -lz     -L/home/xgli/anaconda3/envs/ml/lib -liconv  -lm "
+XML2_INCLUDEDIR="-I/home/xgli/anaconda3/envs/ml/include/libxml2 -I/home/xgli/anaconda3/envs/ml/include"
+MODULE_VERSION="xml2-2.9.4"
+
